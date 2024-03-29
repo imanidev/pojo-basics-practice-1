f6f96b5b90f9dedaf3970e8be807de14730b6d58
Imani Roberts <imaniroberts.dev@gmail.com>
2024-03-28 21:09:57 -0400

    solved

--- problems/04-get-full-name.js
+++ problems/04-get-full-name.js
@@ -6,13 +6,24 @@ and returns a string containing their full name.
 Examples:
 let p1 = {firstName: 'John', lastName: 'Doe'};
 getFullName(p1); // => 'John Doe'
-let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
-getFullName(p2); // => 'Charlie Brown'
 ***********************************************************************/
 
+
+
 function getFullName(person) {
-  // Your code here 
+  let arrValues = Object.values(p2);
+  let name = [];
+  for (let i = 0; i < arrValues.length; i++) {
+    let value = arrValues[i];
+    if (typeof value === "string") {
+      name.push(value);
+    }
+  }
+  return name.join(' ') ;
 }
+let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
+console.log(getFullName(p2)); // => 'Charlie Brown'
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = getFullName;
